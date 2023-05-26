79caabaf8f8e9393e954ccf006894f81ac762fd6
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-05-26 10:51:19 +0700

    Finished key rotation tools

--- app/Jobs/KeyRotationJob.php
+++ app/Jobs/KeyRotationJob.php
@@ -9,6 +9,7 @@ use Illuminate\Encryption\Encrypter;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
@@ -38,11 +39,28 @@ class KeyRotationJob implements ShouldQueue
                 $oldKey = base64_decode(Str::after($oldKey, 'base64:'));
             }
 
+            $newKey = config('app.key');
+            if (Str::startsWith($newKey, 'base64:')) {
+                $newKey = base64_decode(Str::after($newKey, 'base64:'));
+            }
+
             $decrypterInstance = new Encrypter($oldKey, config('app.cipher'));
+            $encrypterInstance = new Encrypter($newKey, config('app.cipher'));
 
             try {
-                DB::transaction(function () {
-                    // Update encrypted data with new key
+                DB::transaction(function () use ($decrypterInstance, $encrypterInstance) {
+                    /** Update Encrypted Data with New Key must use DB Facade not Eloquent to prevent call to CAST */
+                    DB::table('users')->chunkById(10000, function (Collection $users) use ($decrypterInstance, $encrypterInstance) {
+                        foreach ($users as $user) {
+
+                            (array) $updatedData = [];
+                            if (! is_null($user->totp_key)) {
+                                $updatedData['totp_key'] = $encrypterInstance->encrypt($decrypterInstance->decrypt($user->totp_key, false), false);
+                            }
+
+                            DB::table('users')->where('id', $user->id)->update($updatedData);
+                        }
+                    });
                 });
             } catch (DecryptException $e) {
                 Log::debug('Job Error', ['jobName' => 'KeyRotationJob', 'message' => 'Error decrypting data with old key.']);

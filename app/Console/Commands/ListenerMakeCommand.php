de54b4cec04bcb80b2ff545b995fe738fe882ad2
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-07-12 10:35:52 +0700

    Add custom listener command

---
+++ app/Console/Commands/ListenerMakeCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Foundation\Console\ListenerMakeCommand as BaseListenerMakeCommand;
+
+class ListenerMakeCommand extends BaseListenerMakeCommand
+{
+    /**
+     * Resolve the fully-qualified path to the stub.
+     *
+     * @param  string  $stub
+     * @return string
+     */
+    protected function resolveStubPath($stub)
+    {
+        return file_exists($customPath = $this->laravel->basePath(trim($stub, '/')))
+                        ? $customPath
+                        : __DIR__.$stub;
+    }
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        if ($this->option('queued')) {
+            return $this->option('event')
+                        ? $this->resolveStubPath('/stubs/listener-queued.stub')
+                        : $this->resolveStubPath('/stubs/listener-queued-duck.stub');
+        }
+
+        return $this->option('event')
+                    ? $this->resolveStubPath('/stubs/listener.stub')
+                    : $this->resolveStubPath('/stubs/listener-duck.stub');
+    }
+}

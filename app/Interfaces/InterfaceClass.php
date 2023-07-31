afcd95ff0e13406d40191fee9f5e56821d9954ad
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-07-31 14:42:43 +0700

    Add local version hash

--- app/Interfaces/InterfaceClass.php
+++ app/Interfaces/InterfaceClass.php
@@ -7,4 +7,20 @@ use App\Traits\CommonFunction;
 class InterfaceClass implements CacheKeyConst, PermissionConst, ResetPassConst
 {
     use CommonFunction;
+
+    public function __construct()
+    {
+        //
+    }
+
+    public static function readApplicationVersion(): string
+    {
+        $file = file_get_contents(base_path('.constants'));
+        
+        foreach (explode("\n", $file) as $line) {
+            if (strpos($line, 'APP_VERSION_HASH') !== false) {
+                return str_replace('APP_VERSION_HASH=', '', $line);
+            }
+        }
+    }
 }

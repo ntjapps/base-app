b419a24a4615189027a782b15ee542520826d263
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-09-25 16:56:44 +0700

    Use pass through

--- resources/vue/AppVue.ts
+++ resources/vue/AppVue.ts
@@ -2,6 +2,7 @@ import { createApp, App } from "vue";
 import { createPinia, Pinia } from "pinia";
 const pinia: Pinia = createPinia();
 import PrimeVue from "primevue/config";
+import { usePassThrough } from "primevue/passthrough";
 import Tailwind from "primevue/passthrough/tailwind";
 
 /** Vue router needed for navigation menu */
@@ -12,11 +13,16 @@ import DialogService from "primevue/dialogservice";
 import ToastService from "primevue/toastservice";
 import Tooltip from "primevue/tooltip";
 
+const CustomTailwind = usePassThrough(Tailwind, {
+    mergeSections: true,
+    mergeProps: false,
+});
+
 // Mount Application Instances
 const MainApp: App<Element> = createApp({})
     .use(router)
     .use(pinia)
-    .use(PrimeVue, { unstyled: true, pt: Tailwind })
+    .use(PrimeVue, { ripple: true, unstyled: true, pt: CustomTailwind })
     .use(DialogService)
     .use(ToastService)
     .directive("tooltip", Tooltip);

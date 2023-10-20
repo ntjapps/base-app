729b4a5b497eaadd04e6012b1d26b8f9edfcdd7b
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-10-20 08:01:46 +0700

    Skip if log exists

--- database/migrations/2014_10_12_000000_create_log_table.php
+++ database/migrations/2014_10_12_000000_create_log_table.php
@@ -11,6 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (Schema::hasTable('log')) {
+            return;
+        }
+
         Schema::create('log', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->text('message');

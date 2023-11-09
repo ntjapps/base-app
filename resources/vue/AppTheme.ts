38dcf935459bf95e3e47be6f140253301cde567a
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-11-09 16:38:49 +0700

    Import component

--- resources/vue/AppTheme.ts
+++ resources/vue/AppTheme.ts
@@ -3,6 +3,7 @@ import dropdown from "./PrimevueThemes/dropdown";
 import fileupload from "./PrimevueThemes/fileupload";
 import global from "./PrimevueThemes/global";
 import inputtext from "./PrimevueThemes/inputtext";
+import panelmenu from "./PrimevueThemes/panelmenu";
 import password from "./PrimevueThemes/password";
 import progressspiner from "./PrimevueThemes/progressspinner";
 import tieredmenu from "./PrimevueThemes/tieredmenu";
@@ -14,6 +15,7 @@ export const AppTheme = {
     fileupload: fileupload,
     global: global,
     inputtext: inputtext,
+    panelmenu: panelmenu,
     password: password,
     progressspiner: progressspiner,
     tieredmenu: tieredmenu,

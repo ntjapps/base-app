ec5301001e5dbd2bf0c874c8e67def47d4874fcf
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-11-11 22:09:10 +0700

    Panel menu updated

--- resources/vue/PrimevueThemes/panelmenu.ts
+++ resources/vue/PrimevueThemes/panelmenu.ts
@@ -22,36 +22,35 @@ export default {
     },
     headercontent: {
         class: [
-            "border border-solid border-gray-300 dark:border-blue-900/40 text-gray-700 dark:text-white/80 bg-gray-100 dark:bg-gray-900 rounded-md transition-shadow duration-200",
-            "hover:bg-gray-200 dark:hover:bg-gray-800/80  hover:text-gray-700 dark:hover:text-white/80",
+            "border border-solid rounded-md transition-shadow duration-200",
+            "hover:bg-primary",
         ],
     },
     headeraction: {
         class: [
             "flex items-center select-none cursor-pointer relative no-underline",
-            "text-gray-700 dark:text-white/80 p-5 font-bold",
+            "p-5 font-bold",
         ],
     },
     submenuicon: "mr-2",
     headericon: "mr-2",
     menucontent:
-        "py-1 border border-t-0 border-gray-300 dark:border-blue-900/40 bg-white dark:bg-gray-900 text-gray-700 dark:text-white/80 rounded-t-none rounded-br-md rounded-bl-md",
+        "py-1 border border-t-0 rounded-t-none rounded-br-md rounded-bl-md",
     menu: {
         class: ["outline-none", "m-0 p-0 list-none"],
     },
     content: ({ context }: { context: any }) => ({
         class: [
-            "text-gray-700 dark:text-white/80 transition-shadow duration-200 border-none rounded-none",
-            "hover:bg-gray-200 dark:hover:bg-gray-800/80  hover:text-gray-700 dark:hover:text-white/80", // Hover
+            "transition-shadow duration-200 border-none rounded-none",
+            "hover:bg-primary", // Hover
             {
-                "bg-gray-300 text-gray-700 dark:text-white/80 dark:bg-gray-800/90":
-                    context.focused,
-            },
+                "bg-primary-focus": context.focused,
+            }
         ],
     }),
     action: {
         class: [
-            "text-gray-700 dark:text-white/80 py-3 px-5 select-none",
+            "py-3 px-5 select-none",
             "flex items-center cursor-pointer no-underline relative overflow-hidden",
         ],
     },

f891c93c4ec8e3d38bd7da81374f1efca37331b7
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-11-29 14:46:04 +0700

    Add import for app.js and update global styles

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -3,6 +3,7 @@ import './commands'
 
 import { mount } from 'cypress/vue'
 // Ensure global styles are loaded
+import '../../resources/ts/app.js'
 import '../../resources/css/app.scss'
 
 Cypress.Commands.add('mount', (component, options) => {

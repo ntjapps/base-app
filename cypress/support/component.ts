0c935a1524d8010aabb1b6c9ef73d316077e16fc
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-11-29 14:34:44 +0700

    Add Cypress test for CmpTesting.vue component

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -1,14 +1,10 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
-// Alternatively you can use CommonJS syntax:
-// require('./commands')
-
 import { mount } from 'cypress/vue'
 // Ensure global styles are loaded
 import '../../resources/css/app.scss'
 
-Cypress.Commands.add('mount', mount)
-
-// Example use:
-// cy.mount(MyComponent)
+Cypress.Commands.add('mount', (component, options) => {
+    return mount(component, options)
+});

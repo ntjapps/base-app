bdcf9e06c90d8bc26c0ed00693a9f2896261e3e5
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-12-05 11:19:53 +0700

    Fix Cypress commands and add session management

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,4 +1,4 @@
-/// <reference types="cypress" />
+// <reference focus().types="cypress" />
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
@@ -18,9 +18,64 @@
 //   namespace Cypress {
 //     interface Chainable {
 //       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+//       drag(subject: string, options?: Partial<focus().TypeOptions>): Chainable<Element>
+//       dismiss(subject: string, options?: Partial<focus().TypeOptions>): Chainable<Element>
 //       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
 //     }
 //   }
 // }
+
+declare global {
+    namespace Cypress {
+        interface Chainable {
+            saveLocalStorage(key: string): Chainable<void>;
+            restoreLocalStorage(key: string): Chainable<void>;
+            login(username: string): Chainable<void>;
+        }
+    }
+}
+
+let LOCAL_STORAGE_DATA = {};
+
+Cypress.Commands.add("saveLocalStorage", () => {
+    Object.keys(localStorage).forEach((key) => {
+        LOCAL_STORAGE_DATA[key] = localStorage[key];
+    });
+});
+
+Cypress.Commands.add("restoreLocalStorage", () => {
+    Object.keys(LOCAL_STORAGE_DATA).forEach((key) => {
+        localStorage.setItem(key, LOCAL_STORAGE_DATA[key]);
+    });
+});
+
+Cypress.Commands.add("login", (username: string) => {
+    cy.session(
+        username,
+        () => {
+            cy.visit("/");
+            cy.get("body").should("contain", "Login");
+
+            //Modify this line to your own login method
+            cy.get('button[id="btn-switch-login-mode-to-password"').click();
+
+            cy.get('input[id="username"]').as("username");
+            cy.get("@username").should("exist");
+            cy.get("@username").focus().type(username);
+            cy.get('div[id="password"]').find("input").first().as("password");
+            cy.get("@password").focus().type("password");
+
+            cy.get('input[name="cf-turnstile-response"]')
+                .should("exist")
+                .should("have.attr", "value", "XXXX.DUMMY.TOKEN.XXXX");
+
+            cy.get('button[id="btn-login"]').click();
+            cy.url().should("contain", "/");
+        },
+        {
+            validate() {
+                cy.document().its("cookie").should("contain", "XSRF-TOKEN");
+            },
+        },
+    );
+});
--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,20 +1,32 @@
+import "./commands.js";
+
 before(() => {
     cy.exec(
-        "if [ -f .env.cypress.backup ]; then echo 'It seems Cypress did not finish last time. Check the .env file to prevent loss of data.'; cp .env.cypress.backup .env; rm .env.cypress.backup; fi"
+        "if [ -f .env.cypress.backup ]; then echo 'It seems Cypress did not finish last time. Check the .env file to prevent loss of data.'; cp .env.cypress.backup .env; rm .env.cypress.backup; fi",
     );
     cy.exec("echo 'Cypress started. Backing up .env file.'");
     cy.exec("cp .env .env.cypress.backup");
     cy.exec("cp .env.testing .env");
 
-    cy.exec("echo 'Cypress started. Installing dependencies.'");
-    cy.exec("npm run build");
+    Cypress.session.clearAllSavedSessions();
 });
 
 after(() => {
+    cy.exec("echo 'Cypress finished. Removing session.'");
+
     cy.exec("cp .env.cypress.backup .env");
     cy.exec("rm .env.cypress.backup");
     cy.exec("echo 'Cypress finished. Restored .env file.'");
 
-    cy.exec("echo 'Cypress finished. Removing dependencies.'");
-    cy.exec("rm -rf public/build");
+    cy.clearAllLocalStorage();
+    cy.clearAllCookies();
+    cy.clearAllSessionStorage();
+});
+
+beforeEach(() => {
+    cy.restoreLocalStorage("auth");
+});
+
+afterEach(() => {
+    cy.saveLocalStorage("auth");
 });

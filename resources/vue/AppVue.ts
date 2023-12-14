e9a92404847c46b49efcdef0ad1236fd9c0c9027
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-12-14 09:14:22 +0000

    Add Sentry for error tracking in Vue and Laravel

--- resources/vue/AppVue.ts
+++ resources/vue/AppVue.ts
@@ -3,6 +3,7 @@ import { createPinia, Pinia } from "pinia";
 const pinia: Pinia = createPinia();
 import PrimeVue from "primevue/config";
 import Tailwind from "primevue/passthrough/tailwind";
+import * as Sentry from "@sentry/vue";
 
 /** Vue router needed for navigation menu */
 import { router } from "./AppRouter";
@@ -11,6 +12,23 @@ import { router } from "./AppRouter";
 import DialogService from "primevue/dialogservice";
 import ToastService from "primevue/toastservice";
 import Tooltip from "primevue/tooltip";
+
+/** Sentry iniitialization */
+Sentry.init({
+    dsn: import.meta.env.VITE_SENTRY_DSN,
+    integrations: [
+        new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", import.meta.env.VITE_APP_URL],
+        }),
+        new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, //  Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 // Mount Application Instances
 const MainApp: App<Element> = createApp({})

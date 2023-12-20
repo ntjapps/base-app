9342777f5b0746804114482167cbf10a0807f4c1
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2023-12-20 02:32:09 +0000

    Update logging configuration to use daily log rotation with 7 days retention

--- config/logging.php
+++ config/logging.php
@@ -60,7 +60,7 @@ return [
 
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['database', 'stderr'],
+            'channels' => ['database', 'daily'],
             'ignore_exceptions' => false,
         ],
 
@@ -75,7 +75,7 @@ return [
             'driver' => 'daily',
             'path' => storage_path('logs/laravel.log'),
             'level' => env('LOG_LEVEL', 'debug'),
-            'days' => 14,
+            'days' => 7,
             'replace_placeholders' => true,
         ],
 

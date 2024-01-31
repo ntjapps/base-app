7d09bde09a2b3146a3d19e6eb252d51c3e0486ff
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-01-31 15:51:04 +0000

    Add turnstile test

---
+++ tests/Feature/TurnstileApiTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Traits\Turnstile;
+use Illuminate\Support\Facades\Http;
+use Tests\TestCase;
+
+class TurnstileApiTest extends TestCase
+{
+    use Turnstile;
+
+    /**
+     * Test verify challenge method.
+     */
+    public function test_verify_challenge_method(): void
+    {
+        Http::fake([
+            config('challenge.url') => Http::response([
+                'success' => true,
+            ]),
+        ]);
+        
+        $this->assertTrue($this->verifyChallenge('test'));
+    }
+
+    /**
+     * Test verify mobile challenge method.
+     */
+    public function test_verify_mobile_challenge_method(): void
+    {
+        Http::fake([
+            config('challenge.url') => Http::response([
+                'success' => true,
+            ]),
+        ]);
+
+        $this->assertTrue($this->verifyMobileChallenge('test'));
+    }
+}

36f14564d8b32b0711ba5a3a950c0d7d90c46a5e
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-02-19 02:12:44 +0000

    Updated cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     videosFolder: "tests/cypress/output/videos",
     video: true,
     e2e: {
-        baseUrl: "http://finacc.docker.localhost",
+        baseUrl: "http://docker.localhost",
         supportFile: "tests/cypress/support/e2e.{js,jsx,ts,tsx}",
         specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     },

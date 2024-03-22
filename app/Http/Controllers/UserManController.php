28d023d04f7fe306a84657ca2b026ef98b14254e
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-03-22 23:54:55 +0000

    Fixed missing attributes

--- app/Http/Controllers/UserManController.php
+++ app/Http/Controllers/UserManController.php
@@ -57,7 +57,6 @@ class UserManController extends Controller
                 'permissions_array' => Cache::tags([Permission::class])->remember(Permission::class.'-getPermissions-'.$user->id, Carbon::now()->addYear(), function () use ($user) {
                     return $user->getAllPermissions()->sortBy([
                         ['is_const', 'desc'],
-                        ['is_menu', 'asc'],
                         ['name', 'asc'],
                     ])->pluck('name');
                 }),

8689a2759895bebb85525f4e471b70a6f55d2f1d
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-03-26 23:27:54 +0000

    More often schedules

--- routes/console/TaskSchedules.php
+++ routes/console/TaskSchedules.php
@@ -5,30 +5,30 @@ use Illuminate\Support\Facades\Schedule;
 
 /** Packages Cron */
 Schedule::command('model:prune')->everyMinute()->withoutOverlapping();
-Schedule::command('queue:prune-failed')->hourly()->withoutOverlapping();
-Schedule::command('queue:prune-batches')->hourly()->withoutOverlapping();
-Schedule::command('queue:flush')->hourly()->withoutOverlapping();
+Schedule::command('queue:prune-failed')->everyMinute()->withoutOverlapping();
+Schedule::command('queue:prune-batches')->everyMinute()->withoutOverlapping();
+Schedule::command('queue:flush')->everyMinute()->withoutOverlapping();
 
 if (class_exists(\Laravel\Horizon\HorizonServiceProvider::class)) {
     Schedule::command('horizon:snapshot')->everyFiveMinutes()->withoutOverlapping();
 }
 
 if (class_exists(\Laravel\Passport\PassportServiceProvider::class)) {
-    Schedule::command('passport:purge')->hourly()->withoutOverlapping();
+    Schedule::command('passport:purge')->everyMinute()->withoutOverlapping();
 }
 
 if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
-    Schedule::command('telescope:prune')->hourly()->withoutOverlapping();
+    Schedule::command('telescope:prune')->everyMinute()->withoutOverlapping();
 }
 
 if (class_exists(\Laravel\Pulse\PulseServiceProvider::class)) {
     Schedule::command('pulse:check', ['--once'])->everyThirtySeconds()->withoutOverlapping();
     Schedule::command('pulse:work', ['--stop-when-empty'])->everyTenSeconds()->withoutOverlapping(); /** Must be more frequent than pulse:check */
-    Schedule::command('pulse:clear', ['--type=cpu,memory,system'])->hourly()->withoutOverlapping();
+    Schedule::command('pulse:clear', ['--type=cpu,memory,system'])->everyFifteenMinutes()->withoutOverlapping();
 }
 
 if (config('cache.default') === 'redis') {
-    Schedule::command('cache:prune-stale-tags')->hourly()->withoutOverlapping();
+    Schedule::command('cache:prune-stale-tags')->everyMinute()->withoutOverlapping();
 }
 
 /** Custom Jobs Cron */

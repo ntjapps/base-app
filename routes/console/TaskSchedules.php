abeaf6de73c7b6f850f9ffaefcd206dab17f7c19
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-05-10 23:51:33 +0000

    Removed without overlaping

--- routes/console/TaskSchedules.php
+++ routes/console/TaskSchedules.php
@@ -4,31 +4,31 @@ use App\Jobs\PruneLogDebugLevelJob;
 use Illuminate\Support\Facades\Schedule;
 
 /** Packages Cron */
-Schedule::command('model:prune')->everyMinute()->withoutOverlapping();
-Schedule::command('queue:prune-failed')->everyMinute()->withoutOverlapping();
-Schedule::command('queue:prune-batches')->everyMinute()->withoutOverlapping();
-Schedule::command('queue:flush')->everyMinute()->withoutOverlapping();
+Schedule::command('model:prune')->everyMinute();
+Schedule::command('queue:prune-failed')->everyMinute();
+Schedule::command('queue:prune-batches')->everyMinute();
+Schedule::command('queue:flush')->everyMinute();
 
 if (class_exists(\Laravel\Horizon\HorizonServiceProvider::class)) {
-    Schedule::command('horizon:snapshot')->everyFiveMinutes()->withoutOverlapping();
+    Schedule::command('horizon:snapshot')->everyFiveMinutes();
 }
 
 if (class_exists(\Laravel\Passport\PassportServiceProvider::class)) {
-    Schedule::command('passport:purge')->everyMinute()->withoutOverlapping();
+    Schedule::command('passport:purge')->everyMinute();
 }
 
 if (class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
-    Schedule::command('telescope:prune')->everyFifteenMinutes()->withoutOverlapping();
+    Schedule::command('telescope:prune')->everyFifteenMinutes();
 }
 
 if (class_exists(\Laravel\Pulse\PulseServiceProvider::class)) {
-    Schedule::command('pulse:check', ['--once'])->everyThirtySeconds()->withoutOverlapping();
-    Schedule::command('pulse:work', ['--stop-when-empty'])->everyTenSeconds()->withoutOverlapping(); /** Must be more frequent than pulse:check */
-    Schedule::command('pulse:clear', ['--type=cpu,memory,system'])->everyFifteenMinutes()->withoutOverlapping();
+    Schedule::command('pulse:check', ['--once'])->everyThirtySeconds();
+    Schedule::command('pulse:work', ['--stop-when-empty'])->everyTenSeconds(); /** Must be more frequent than pulse:check */
+    Schedule::command('pulse:clear', ['--type=cpu,memory,system'])->everyFifteenMinutes();
 }
 
 if (config('cache.default') === 'redis') {
-    Schedule::command('cache:prune-stale-tags')->everyMinute()->withoutOverlapping();
+    Schedule::command('cache:prune-stale-tags')->everyMinute();
 }
 
 /** Custom Jobs Cron */

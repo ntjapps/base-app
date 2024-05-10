256d3d1b7cdbaa4c96648535c2ba8907d06be0b6
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-05-10 06:12:04 +0000

    Updated CSS Presets

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -23,6 +23,18 @@ module.exports = {
     theme: {
         extend: {
             colors: {
+                primary: "rgb(var(--primary))",
+                "primary-inverse": "rgb(var(--primary-inverse))",
+                "primary-hover": "rgb(var(--primary-hover))",
+                "primary-active-color": "rgb(var(--primary-active-color))",
+
+                "primary-highlight":
+                    "rgb(var(--primary)/var(--primary-highlight-opacity))",
+                "primary-highlight-inverse":
+                    "rgb(var(--primary-highlight-inverse))",
+                "primary-highlight-hover":
+                    "rgb(var(--primary)/var(--primary-highlight-hover-opacity))",
+
                 "primary-50": "rgb(var(--primary-50))",
                 "primary-100": "rgb(var(--primary-100))",
                 "primary-200": "rgb(var(--primary-200))",
@@ -34,6 +46,7 @@ module.exports = {
                 "primary-800": "rgb(var(--primary-800))",
                 "primary-900": "rgb(var(--primary-900))",
                 "primary-950": "rgb(var(--primary-950))",
+
                 "surface-0": "rgb(var(--surface-0))",
                 "surface-50": "rgb(var(--surface-50))",
                 "surface-100": "rgb(var(--surface-100))",

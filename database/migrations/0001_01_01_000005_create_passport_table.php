7afc2e8afa2b559cfd20d8b0c2e558fd503d2fe5
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-07-01 13:47:08 +0000

    Deps update

--- database/migrations/0001_01_01_000005_create_passport_table.php
+++ database/migrations/0001_01_01_000005_create_passport_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
                 $table->string('name');
                 $table->string('secret', 100)->nullable();
                 $table->string('provider')->nullable();
-                $table->text('redirect');
+                $table->text('redirect')->nullable();
                 $table->boolean('personal_access_client');
                 $table->boolean('password_client');
                 $table->boolean('revoked');

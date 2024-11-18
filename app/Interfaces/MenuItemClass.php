7ea9b9e7e298c21e3929631181a9053891937058
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2024-11-18 04:58:07 +0000

    Simpler interface

--- app/Interfaces/MenuItemClass.php
+++ app/Interfaces/MenuItemClass.php
@@ -2,11 +2,9 @@
 
 namespace App\Interfaces;
 
-use App\Features\DevSystem;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Pennant\Feature;
 
 class MenuItemClass
 {

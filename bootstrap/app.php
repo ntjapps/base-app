ff8b37091229db639402fc75a07e71ae21b8e848
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-01-02 03:21:56 +0000

    Add trust proxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,6 +21,19 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/app/healthcheck',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: [
+            '127.0.0.0/8',
+            '10.0.0.0/8',
+            '172.16.0.0/12',
+            '192.168.0.0/16',
+        ]);
+
+        $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
 
         $middleware->web(append: [
             CreateFreshApiToken::class,

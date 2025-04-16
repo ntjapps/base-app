be65e0386f3e33925c6cdff50580b89b9efb93ae
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-04-16 10:33:23 +0000

    feat: Replace PanelMenu with custom MenuPanel component for improved functionality

--- resources/vue/Components/CmpMenu.vue
+++ resources/vue/Components/CmpMenu.vue
@@ -3,7 +3,7 @@ import { storeToRefs } from 'pinia';
 
 import { useMainStore } from '../AppState';
 
-import PanelMenu from 'primevue/panelmenu';
+import MenuPanel from '../volt/MenuPanel.vue';
 
 const main = useMainStore();
 const { appName, menuItems, expandedKeysMenu } = storeToRefs(main);
@@ -18,7 +18,7 @@ const { appName, menuItems, expandedKeysMenu } = storeToRefs(main);
                 </div>
             </div>
             <div class="flex w-full mt-10">
-                <PanelMenu v-model:expandedKeys="expandedKeysMenu" :model="menuItems" />
+                <MenuPanel v-model:expandedKeys="expandedKeysMenu" :model="menuItems" />
             </div>
         </div>
     </div>

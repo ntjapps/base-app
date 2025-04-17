1fd161e291b3de3dabe660fddfe068ce41defbac
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-04-17 02:55:48 +0000

    feat: Add Message component to PgProfile for password guidance and update styles

--- resources/vue/DashboardPages/PgProfile.vue
+++ resources/vue/DashboardPages/PgProfile.vue
@@ -9,6 +9,7 @@ import CmpToast from '../Components/CmpToast.vue';
 
 import InputText from '../volt/InputText.vue';
 import Password from '../volt/Password.vue';
+import Message from '../volt/Message.vue';
 
 const props = defineProps<{
     appName: string;
@@ -63,49 +64,48 @@ onMounted(() => {
         <div class="my-3 mx-5 p-5 bg-surface-200 rounded-lg drop-shadow-lg">
             <h3 class="title-font">Update profile in {{ appName }}</h3>
             <div class="mt-10 mb-5">
-                <span class="p-float-label w-full">
-                    <InputText
-                        id="name"
-                        v-model="userName"
-                        type="text"
-                        class="w-full"
-                        @keyup.enter="postProfileData"
-                    />
-                    <label class="w-full" for="name">Name</label>
-                </span>
+                <label for="name">Name</label>
+                <InputText
+                    id="name"
+                    v-model="userName"
+                    type="text"
+                    class="w-full"
+                    @keyup.enter="postProfileData"
+                />
             </div>
             <div class="mt-10 mb-5">
-                <span class="p-float-label w-full">
-                    <Password
-                        id="newpassword"
-                        v-model="newPassword"
-                        class="w-full"
-                        inputClass="w-full"
-                        toggleMask
-                        @keyup.enter="postProfileData"
-                    />
-                    <label class="w-full" for="newpas"
-                        >New Password (Must be filled if changing password, leave empty if don't
-                        want to change password)</label
-                    >
-                </span>
+                <label class="w-full" for="newpas">New Password</label>
+                <Password
+                    id="newpassword"
+                    v-model="newPassword"
+                    class="w-full"
+                    inputClass="w-full"
+                    toggleMask
+                    @keyup.enter="postProfileData"
+                />
+                <Message
+                    >Must be filled if changing password, leave empty if don't want to change
+                    password</Message
+                >
             </div>
             <div class="mt-10 mb-5">
-                <span class="p-float-label w-full">
-                    <Password
-                        id="confirmpassword"
-                        v-model="confirmPassword"
-                        class="w-full"
-                        inputClass="w-full"
-                        toggleMask
-                        :feedback="false"
-                        @keyup.enter="postProfileData"
-                    />
-                    <label class="w-full" for="confi"
-                        >Confirm Password (Must be filled if changing password, leave empty if don't
-                        want to change password)</label
-                    >
-                </span>
+                <label class="w-full" for="confi"
+                    >Confirm Password (Must be filled if changing password, leave empty if don't
+                    want to change password)</label
+                >
+                <Password
+                    id="confirmpassword"
+                    v-model="confirmPassword"
+                    class="w-full"
+                    inputClass="w-full"
+                    toggleMask
+                    :feedback="false"
+                    @keyup.enter="postProfileData"
+                />
+                <Message
+                    >Must be filled if changing password, leave empty if don't want to change
+                    password</Message
+                >
             </div>
             <div class="flex justify-center">
                 <UButton size="xl" label="Update Profile" @click="postProfileData" />

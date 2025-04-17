ad0cf9aa85b1107a31978667b24edd920db35c01
Nathanael Yusuf Tj <26967808+nathanaelytj@users.noreply.github.com>
2025-04-17 02:41:58 +0000

    feat: Introduce DrawerLeft component and update CmpMobileMenu to use it

--- resources/vue/Components/CmpMobileMenu.vue
+++ resources/vue/Components/CmpMobileMenu.vue
@@ -4,7 +4,7 @@ import { storeToRefs } from 'pinia';
 import { useMainStore } from '../AppState';
 
 import MenuPanel from '../volt/MenuPanel.vue';
-import Drawer from '../volt/Drawer.vue';
+import Drawer from '../volt/DrawerLeft.vue';
 
 const main = useMainStore();
 const { menuItems, menuVisible } = storeToRefs(main);
--- resources/vue/volt/DrawerLeft.vue
+++ resources/vue/volt/DrawerLeft.vue
@@ -33,21 +33,17 @@ const theme = ref<DrawerPassThroughOptions>({
         bg-surface-0 dark:bg-surface-900
         text-surface-700 dark:text-surface-0 
         shadow-[0_20px_25px_-5px_rgba(0,0,0,0.1),0_8px_10px_-6px_rgba(0,0,0,0.1)]
-        p-left:w-80 p-left:h-full p-left:border-r
-        p-right:w-80 p-right:h-full p-right:border-s
-        p-top:h-40 p-top:w-full p-top:border-b
-        p-bottom:h-40 p-bottom:w-full p-bottom:border-t
-        p-full-screen:transition-opacity p-full-screen:transform-none p-full-screen:w-screen p-full-screen:h-screen p-full-screen:max-h-full p-full-screen:top-0 p-full-screen:left-0`,
+        w-80 h-full border-r`,
     header: `flex items-center justify-between flex-shrink-0 p-5`,
     title: `font-semibold text-2xl`,
     content: `overflow-y-auto flex-grow pt-0 pb-5 px-5`,
     footer: `p-5`,
     mask: `p-modal:bg-black/50`,
     transition: {
-        enterFromClass: `p-left:-translate-x-full p-right:translate-x-full p-top:-translate-y-full p-bottom:translate-y-full p-full-screen:opacity-0`,
-        enterActiveClass: `transition-transform duration-400 ease-out p-full-screen:transition-opacity`,
-        leaveActiveClass: `transition-transform duration-200 ease-in p-full-screen:transition-opacity`,
-        leaveToClass: `p-left:-translate-x-full p-right:translate-x-full p-top:-translate-y-full p-bottom:translate-y-full p-full-screen:opacity-0`,
+        enterFromClass: `-translate-x-full`,
+        enterActiveClass: `transition-transform duration-400 ease-out`,
+        leaveActiveClass: `transition-transform duration-200 ease-in`,
+        leaveToClass: `-translate-x-full`,
     },
 });
 </script>

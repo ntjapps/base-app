9961fbf26f49d906a6cd4615753afdfd7a35cf85
Nathanael Yusuf Tjahjanadi <nathanaelytj@ntjapps.com>
2025-10-17 16:05:29 +0700

    Update BasePolicy.php
    Updated to be DRY
    
    Signed-off-by: Nathanael Yusuf Tjahjanadi <nathanaelytj@ntjapps.com>

--- app/Policies/BasePolicy.php
+++ app/Policies/BasePolicy.php
@@ -12,21 +12,37 @@ use Illuminate\Support\Facades\Cache;
 trait BasePolicy
 {
     /**
+     * Check if user has a specific permission by ability title
+     */
+    protected function hasPermissionByAbility(User $user, string $abilityTitle): bool
+    {
+        $permission = Cache::remember(
+            CentralCacheInterfaceClass::keyPermissionAbility($abilityTitle),
+            Carbon::now()->addYear(),
+            function () use ($abilityTitle) {
+                return Permission::whereHas('ability', function ($query) use ($abilityTitle) {
+                    return $query->where('title', $abilityTitle);
+                })->first();
+            }
+        );
+
+        if (!$permission) {
+            return false;
+        }
+
+        return Cache::remember(
+            CentralCacheInterfaceClass::keyPermissionHasPermissionTo($permission->id, $user->id),
+            Carbon::now()->addYear(),
+            fn() => $user->hasPermissionTo($permission)
+        );
+    }
+
+    /**
      * Check if user has Super User permission
      */
-    public function hasSuperPermission(User $user): ?bool
+    public function hasSuperPermission(User $user): bool
     {
-        $permission = Cache::remember(CentralCacheInterfaceClass::keyPermissionAbility(InterfaceClass::SUPER), Carbon::now()->addYear(), function () {
-            return Permission::whereHas('ability', function ($query) {
-                return $query->where('title', InterfaceClass::SUPER);
-            })->first();
-        });
-
-        $hasPermissionToCache = Cache::remember(CentralCacheInterfaceClass::keyPermissionHasPermissionTo($permission->id, $user->id), Carbon::now()->addYear(), function () use ($user, $permission) {
-            return $user->hasPermissionTo($permission);
-        });
-
-        return $hasPermissionToCache ? true : false;
+        return $this->hasPermissionByAbility($user, InterfaceClass::SUPER);
     }
 
     /**
